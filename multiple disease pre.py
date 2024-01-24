8af5608709653f3bd4534fd8c4b2b56a77dfb1a7
Hassan <70138791+Hassan-Mohamed@users.noreply.github.com>
2024-01-24 11:22:30 +0300

    Update multiple disease pre.py

--- multiple disease pre.py
+++ multiple disease pre.py
@@ -19,12 +19,13 @@ def validate_input(input_value, data_type):
 # loading the saved models
 diabetes_model = pickle.load(open('C:/Users/haska/Desktop/Multiple Disease Prediction System/saved models/diabetes_model.sav', 'rb'))
 heart_disease_model = pickle.load(open('C:/Users/haska/Desktop/Multiple Disease Prediction System/saved models/heart_disease_model.sav', 'rb'))
+parkinsons_model = pickle.load(open('C:/Users/haska/Desktop/Multiple Disease Prediction System/saved models/parkinsons_model.sav', 'rb'))
 
 # sidebar for navigation
 with st.sidebar:
     selected = option_menu('Multiple Disease Prediction System',
-                           ['Diabetes Prediction', 'Heart Disease Prediction'],
-                           icons=['activity', 'heart'], default_index=0)
+                           ['Diabetes Prediction', 'Heart Disease Prediction','Parkinsons Prediction'],
+                           icons=['activity', 'heart','person'], default_index=0)
 
 # Diabetes Prediction Page
 if selected == 'Diabetes Prediction':
@@ -128,6 +129,120 @@ if selected == 'Heart Disease Prediction':
             st.error("Please enter Resting Blood Pressure.")
 
 
+# Parkinson's Prediction Page
+if selected == "Parkinsons Prediction":
+    
+    # page title
+    st.title("Parkinson's Disease Prediction using ML")
+    
+    col1, col2, col3, col4, col5 = st.columns(5)  
+    
+    with col1:
+        fo = st.text_input('MDVP:Fo(Hz)')
+        
+    with col2:
+        fhi = st.text_input('MDVP:Fhi(Hz)')
+        
+    with col3:
+        flo = st.text_input('MDVP:Flo(Hz)')
+        
+    with col4:
+        Jitter_percent = st.text_input('MDVP:Jitter(%)')
+        
+    with col5:
+        Jitter_Abs = st.text_input('MDVP:Jitter(Abs)')
+        
+    with col1:
+        RAP = st.text_input('MDVP:RAP')
+        
+    with col2:
+        PPQ = st.text_input('MDVP:PPQ')
+        
+    with col3:
+        DDP = st.text_input('Jitter:DDP')
+        
+    with col4:
+        Shimmer = st.text_input('MDVP:Shimmer')
+        
+    with col5:
+        Shimmer_dB = st.text_input('MDVP:Shimmer(dB)')
+        
+    with col1:
+        APQ3 = st.text_input('Shimmer:APQ3')
+        
+    with col2:
+        APQ5 = st.text_input('Shimmer:APQ5')
+        
+    with col3:
+        APQ = st.text_input('MDVP:APQ')
+        
+    with col4:
+        DDA = st.text_input('Shimmer:DDA')
+        
+    with col5:
+        NHR = st.text_input('NHR')
+        
+    with col1:
+        HNR = st.text_input('HNR')
+        
+    with col2:
+        RPDE = st.text_input('RPDE')
+        
+    with col3:
+        DFA = st.text_input('DFA')
+        
+    with col4:
+        spread1 = st.text_input('spread1')
+        
+    with col5:
+        spread2 = st.text_input('spread2')
+        
+    with col1:
+        D2 = st.text_input('D2')
+        
+    with col2:
+        PPE = st.text_input('PPE')
+    
+    # Check if any of the inputs are empty
+    if st.button("Parkinson's Test Result"):
+        if not any([fo, fhi, flo, Jitter_percent, Jitter_Abs, RAP, PPQ, DDP, Shimmer, Shimmer_dB, APQ3, APQ5, APQ, DDA, NHR, HNR, RPDE, DFA, spread1, spread2, D2, PPE]):
+            st.error("Please fill in all the input fields.")
+        else:
+            # Convert input values to float
+            fo = float(fo)
+            fhi = float(fhi)
+            flo = float(flo)
+            Jitter_percent = float(Jitter_percent)
+            Jitter_Abs = float(Jitter_Abs)
+            RAP = float(RAP)
+            PPQ = float(PPQ)
+            DDP = float(DDP)
+            Shimmer = float(Shimmer)
+            Shimmer_dB = float(Shimmer_dB)
+            APQ3 = float(APQ3)
+            APQ5 = float(APQ5)
+            APQ = float(APQ)
+            DDA = float(DDA)
+            NHR = float(NHR)
+            HNR = float(HNR)
+            RPDE = float(RPDE)
+            DFA = float(DFA)
+            spread1 = float(spread1)
+            spread2 = float(spread2)
+            D2 = float(D2)
+            PPE = float(PPE)
+
+            parkinsons_diagnosis = ''
+            
+            # Create a button for Prediction    
+            parkinsons_prediction = parkinsons_model.predict([[fo, fhi, flo, Jitter_percent, Jitter_Abs, RAP, PPQ, DDP, Shimmer, Shimmer_dB, APQ3, APQ5, APQ, DDA, NHR, HNR, RPDE, DFA, spread1, spread2, D2, PPE]])
+            
+            if parkinsons_prediction[0] == 1:
+                parkinsons_diagnosis = "The person has Parkinson's disease"
+            else:
+                parkinsons_diagnosis = "The person does not have Parkinson's disease"
+            
+            st.success(parkinsons_diagnosis)
 
 
 
